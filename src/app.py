e23e65fbb1d41f2e37b744678750a428090d9d76
ssr-inet <sathyaswaruban@inetcsc.com>
2025-07-01 13:13:10 +0530

    add

--- src/app.py
+++ src/app.py
@@ -123,5 +123,5 @@ def reconciliation():
         return jsonify({"error": "Failed to process data"}), 500
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True, host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
